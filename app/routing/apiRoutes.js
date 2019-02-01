9f2a5cb41975b4073f17c9cdd1c3c4f4c1a70c89
William Harrison <alex@grahamsg.com>
2019-02-01 12:14:27 -0500

    updating homework

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,26 @@
 
-app.get("/api/friends", function(req, res){
-    res.json(friends)
-});
+// var express = require('express');
+// var app = express();
 
-app.post("/api/friends", function(){
+
+var friends = require('../data/friends.js');
+
+
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res){
+        res.json(friends)
+    });
+    
+	app.get('/api/friends', function(req, res) {
+        res.json(friends);
+        
+        
+        app.post("/api/friends", function(){
     var newFriends = req.body;
 
     newFriends.name = newFriends.name.replace(/\s+/g, "").toLowerCase();
     friends.push(newFriends);
 })
+    });
+}
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var path = require("path");
-
+ 
 var app = express();
-var PORT = 5000;
+var PORT = 3000;
+// var PORT= process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 
