45ff16703b8e9be72ee9bb0f6ec3c88986972c2d
William Harrison <alex@grahamsg.com>
2019-02-05 22:54:52 -0500

    update homework

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        name: "Alex",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Zooey",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyN3wUx9HiECtTLZrS8RwUVRrm90_8mG9oz5Qv7V3V7n10GdZu",
         scores: [
             5,
             4,
@@ -14,7 +14,40 @@ var friends = [
             2,
             1
         ]
-    }
+    },
+    {
+        name: "Cameron",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1SIdIcNOM5YtOK-yMV6UdghuPKbd6WC2XsuijMVwXrPjl4IBz",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Ellen",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk08Eci1bP43ajFxQIolkJdYvSjPDZwn6SjHCOMFSAUMcUbYJb",
+        scores: [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,40 @@
 
 var friends = require('../data/friends.js');
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends)
     });
 
-	app.get('/api/friends', function(req, res) {
+    app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
-        
-        app.post("/api/friends", function(){
-    var newFriends = req.body;
 
-    newFriends.name = newFriends.name.replace(/\s+/g, "").toLowerCase();
-    friends.push(newFriends);
-})
-    
+    app.post("/api/friends", function (req, res) {
+        var newFriendsScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            var scoreDif = 0;
+
+            for (var l = 0; l < newFriendsScores.length; l++) {
+                scoreDif += (Math.abs(parseInt(friends[i].scores[l] - parseInt(newFriendsScores[l]))));
+            }
+            scoresArray.push(scoreDif);
+        }
+
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        var match = friends[bestMatch];
+        res.json(match);
+        friends.push(req.body);
+
+    })
+
 }
